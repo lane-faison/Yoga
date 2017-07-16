fcf6d68afd3d22a4900ea2efd2a81bcc93430df2
Lane Faison <lane.faison@icloud.com>
2017-07-15 23:35:15 -0600

    Navigation controllers added to each tabbed page. Profile UI updates.

--- Yoga/ThirdVC.swift
+++ Yoga/ThirdVC.swift
@@ -11,26 +11,6 @@ import Firebase
 
 class ThirdVC: UIViewController {
     
-    let navigationBar: UINavigationBar = {
-        let nav = UINavigationBar()
-        nav.tintColor = .white // Color of the navbar items
-        nav.barTintColor = .green
-        nav.translatesAutoresizingMaskIntoConstraints = false
-        return nav
-    }()
-    
-    let navItem: UINavigationItem = {
-        let item = UINavigationItem()
-        item.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItemStyle.plain, target: nil, action: #selector(handleLogout))
-        return item
-    }()
-    
-    let logoutButton: UIBarButtonItem = {
-        let btn = UIBarButtonItem()
-        btn.title = "Logout"
-        return btn
-    }()
-    
     let userAccountContainerView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.yellow
@@ -71,12 +51,13 @@ class ThirdVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        
+        
         ref = Database.database().reference()
         
-        view.addSubview(navigationBar)
         view.addSubview(userAccountContainerView)
         
-        setupNavigationBar()
         setupUserAccountContainerView()
         
     }
@@ -91,21 +72,10 @@ class ThirdVC: UIViewController {
         }
     }
     
-    func setupNavigationBar() {
-        
-        navigationBar.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        navigationBar.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        navigationBar.heightAnchor.constraint(equalToConstant: 80).isActive = true
-        navigationBar.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        navigationBar.setItems([navItem], animated: false)
-        
-        
-    }
-    
     func setupUserAccountContainerView() {
         
         userAccountContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        userAccountContainerView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor).isActive = true
+        userAccountContainerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 64).isActive = true
         userAccountContainerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         userAccountContainerView.heightAnchor.constraint(equalToConstant: 250).isActive = true
         
