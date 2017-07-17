80028879771afb700b69bb58dd497e5c0d2041bf
Lane Faison <lane.faison@icloud.com>
2017-07-16 23:50:51 -0600

    UINavigationBar and UITabBar colors updated. Profile section UI updates.

--- Yoga/ThirdVC.swift
+++ Yoga/ThirdVC.swift
@@ -13,7 +13,7 @@ class ThirdVC: UIViewController, UIScrollViewDelegate {
     
     let userAccountContainerView: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor.yellow
+        view.backgroundColor = ColorScheme.textOrIcons
         view.layer.masksToBounds = true
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
@@ -21,7 +21,7 @@ class ThirdVC: UIViewController, UIScrollViewDelegate {
     
     let profileImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(named: "avatar")
+        imageView.image = UIImage(named: "star_filled")
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         return imageView
@@ -47,18 +47,73 @@ class ThirdVC: UIViewController, UIScrollViewDelegate {
     
     let userInformationContainerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .gray
+        view.backgroundColor = ColorScheme.textOrIcons
         view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let ratingLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Avenir next", size: 18)
+        label.textAlignment = .left
+        label.textColor = ColorScheme.darkPrimaryColor
+        label.text = "Rating"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let ratingLabelDivider: UIView = {
+        let view = UIView()
+        view.backgroundColor = ColorScheme.lightPrimaryColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let ratingView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let filledStar: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "star_filled")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let emptyStar: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "star_empty")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let bioLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Avenir next", size: 18)
+        label.textAlignment = .left
+        label.textColor = ColorScheme.darkPrimaryColor
+        label.text = "Bio"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let bioLabelDivider: UIView = {
+        let view = UIView()
+        view.backgroundColor = ColorScheme.lightPrimaryColor
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     let bioSection: UITextView = {
         let view = UITextView()
-        view.font = UIFont(name: "Avenir next", size: 12)
-        view.textColor = .black
+        view.font = UIFont(name: "Avenir next", size: 14)
+        view.textColor = ColorScheme.primaryText
         view.textAlignment = .justified
-        view.backgroundColor = .red
+        view.backgroundColor = ColorScheme.textOrIcons
+        view.text = "Here is where my bio will go. This will be a very nice feature to have in your profile. Here is where my bio will go. This will be a very nice feature to have in your profile. Here is where my bio will go. This will be a very nice feature to have in your profile. Here is where my bio will go. This will be a very nice feature to have in your profile."
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -76,7 +131,7 @@ class ThirdVC: UIViewController, UIScrollViewDelegate {
         ref = Database.database().reference()
         
         scrollView = UIScrollView(frame: view.bounds)
-        scrollView.backgroundColor = UIColor.cyan
+        scrollView.backgroundColor = ColorScheme.textOrIcons
         scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: 2000.0)
         scrollView.autoresizingMask = UIViewAutoresizing.flexibleWidth
         scrollView.autoresizingMask = UIViewAutoresizing.flexibleHeight
@@ -86,6 +141,7 @@ class ThirdVC: UIViewController, UIScrollViewDelegate {
         scrollView.addSubview(userInformationContainerView)
         setupUserAccountContainerView()
         setupUserInformationContainerView()
+        handleStarRating()
         view.addSubview(scrollView)
         
     }
@@ -104,7 +160,7 @@ class ThirdVC: UIViewController, UIScrollViewDelegate {
     func setupUserAccountContainerView() {
         
         userAccountContainerView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        userAccountContainerView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 64).isActive = true
+        userAccountContainerView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
         userAccountContainerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
         userAccountContainerView.heightAnchor.constraint(equalToConstant: 250).isActive = true
         
@@ -144,12 +200,50 @@ class ThirdVC: UIViewController, UIScrollViewDelegate {
         userInformationContainerView.heightAnchor.constraint(equalToConstant: 2000).isActive = true
         userInformationContainerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
         
+        userInformationContainerView.addSubview(ratingLabel)
+        userInformationContainerView.addSubview(ratingLabelDivider)
+        userInformationContainerView.addSubview(ratingView)
+        userInformationContainerView.addSubview(bioLabel)
+        userInformationContainerView.addSubview(bioLabelDivider)
         userInformationContainerView.addSubview(bioSection)
         
+        ratingLabel.centerXAnchor.constraint(equalTo: userInformationContainerView.centerXAnchor).isActive = true
+        ratingLabel.widthAnchor.constraint(equalTo: userInformationContainerView.widthAnchor, multiplier: 1.0, constant: -20).isActive = true
+        ratingLabel.topAnchor.constraint(equalTo: userInformationContainerView.topAnchor, constant: 20).isActive = true
+        
+        ratingLabelDivider.centerXAnchor.constraint(equalTo: userInformationContainerView.centerXAnchor).isActive = true
+        ratingLabelDivider.widthAnchor.constraint(equalTo: userInformationContainerView.widthAnchor).isActive = true
+        ratingLabelDivider.topAnchor.constraint(equalTo: ratingLabel.bottomAnchor, constant: 5).isActive = true
+        ratingLabelDivider.heightAnchor.constraint(equalToConstant: 2).isActive = true
+        
+        ratingView.centerXAnchor.constraint(equalTo: userInformationContainerView.centerXAnchor).isActive = true
+        ratingView.widthAnchor.constraint(equalTo: userInformationContainerView.widthAnchor).isActive = true
+        ratingView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        ratingView.topAnchor.constraint(equalTo: ratingLabelDivider.bottomAnchor, constant: 10).isActive = true
+        
+        bioLabel.centerXAnchor.constraint(equalTo: userInformationContainerView.centerXAnchor).isActive = true
+        bioLabel.widthAnchor.constraint(equalTo: userInformationContainerView.widthAnchor, multiplier: 1.0, constant: -20).isActive = true
+        bioLabel.topAnchor.constraint(equalTo: ratingView.bottomAnchor, constant: 20).isActive = true
+        
+        bioLabelDivider.centerXAnchor.constraint(equalTo: userInformationContainerView.centerXAnchor).isActive = true
+        bioLabelDivider.widthAnchor.constraint(equalTo: userInformationContainerView.widthAnchor).isActive = true
+        bioLabelDivider.topAnchor.constraint(equalTo: bioLabel.bottomAnchor, constant: 5).isActive = true
+        bioLabelDivider.heightAnchor.constraint(equalToConstant: 2).isActive = true
+        
         bioSection.centerXAnchor.constraint(equalTo: userInformationContainerView.centerXAnchor).isActive = true
-        bioSection.topAnchor.constraint(equalTo: userInformationContainerView.topAnchor, constant: 20).isActive = true
-        bioSection.widthAnchor.constraint(equalTo: userInformationContainerView.widthAnchor).isActive = true
+        bioSection.topAnchor.constraint(equalTo: bioLabelDivider.bottomAnchor, constant: 10).isActive = true
+        bioSection.widthAnchor.constraint(equalTo: userAccountContainerView.widthAnchor, multiplier: 1.0, constant: -20).isActive = true
         bioSection.heightAnchor.constraint(equalToConstant: 300).isActive = true
+    }
+    
+    func handleStarRating() {
+        ratingView.addSubview(filledStar)
+
+        filledStar.centerXAnchor.constraint(equalTo: ratingView.centerXAnchor, constant: 0).isActive = true
+        filledStar.centerYAnchor.constraint(equalTo: ratingView.centerYAnchor, constant: 0).isActive = true
+        filledStar.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        filledStar.heightAnchor.constraint(equalToConstant: 29).isActive = true
+
     }
     
     func handleLogout() {
