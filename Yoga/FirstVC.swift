ed96cf08f04ad58ab0ea3f70e6cab692f547776f
Lane Faison <lane.faison@icloud.com>
2017-07-17 00:29:14 -0600

    UITableView added on Instructors tab. User data being pulled from Firebase. Still need to add a value so program can distinguish between instructors and students.

--- Yoga/FirstVC.swift
+++ Yoga/FirstVC.swift
@@ -9,15 +9,71 @@
 import UIKit
 import Firebase
 
-class FirstVC: UIViewController {
+class FirstVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    let cellId = "cellId"
+    
+    var instructors = [Instructor]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.title = "Instructors"
+        tableView.delegate = self
+        tableView.dataSource = self
         
+        view.addSubview(tableView)
+        setupTableView()
         
-        
-        
+        tableView.register(InstructorCell.self, forCellReuseIdentifier: cellId)
+        fetchUser()
+    }
+    
+    func setupTableView() {
+        tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        tableView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    func fetchUser() {
+        Database.database().reference().child("users").observe(.childAdded, with: { (snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                let instructor = Instructor()
+                instructor.setValuesForKeys(dictionary)
+                self.instructors.append(instructor)
+                
+                DispatchQueue.main.async(execute: {
+                    self.tableView.reloadData()
+                })
+            }
+            print("User found!")
+            print(snapshot)
+        }, withCancel: nil)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return instructors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let instructor = instructors[indexPath.row]
+        cell.textLabel?.text = instructor.name
+        cell.detailTextLabel?.text = instructor.email
+        return cell
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -40,10 +96,16 @@ class FirstVC: UIViewController {
         present(loginController, animated: true, completion: nil)
     }
     
-    @IBAction func logoutBtnTapped(_ sender: Any) {
-        handleLogout()
+
+}
+
+class InstructorCell: UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
     }
     
-
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
